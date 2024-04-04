9973b5117ae5912ef0f6c7f8a018b08661bb6227
우현 김 <141819642+kimwoozzi@users.noreply.github.com>
2024-04-04 19:59:54 +0900

    Add files via upload

---
+++ WooHyun/src/main/java/com/example/woohyun/WooHyunApplication.java
@@ -0,0 +1,13 @@
+package com.example.woohyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooHyunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WooHyunApplication.class, args);
+    }
+
+}
---
+++ WooHyun/src/test/java/com/example/woohyun/WooHyunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woohyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooHyunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
